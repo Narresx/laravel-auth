369352c8a51269037f1bf0edde15cad17ddeacf4
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-29 17:04:05 +0200

    fix post controller

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -27,7 +27,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -59,7 +59,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return view('admin.posts.show', compact('post'));
+        return view('admin.posts.show');
     }
 
     /**
